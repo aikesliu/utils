3957ae472e44aa15367f8dc1c2a24b19b241d06f
xinliu <xliu@qumitech.com>
2021-06-25 15:05:48 +0800

    fix bug;

--- line/cardinal_spline.go
+++ line/cardinal_spline.go
@@ -1,10 +1,11 @@
 package line
 
-import (
-	"image"
-)
+type FPoint struct {
+	X float64 `json:"x" yaml:"x"`
+	Y float64 `json:"y" yaml:"y"`
+}
 
-func NewCardinalSpline(points []*image.Point, tension float32) *CardinalSpline {
+func NewCardinalSpline(points []*FPoint, tension float64) *CardinalSpline {
 	if len(points) == 0 {
 		return nil
 	}
@@ -12,17 +13,17 @@ func NewCardinalSpline(points []*image.Point, tension float32) *CardinalSpline {
 		points:  points,
 		tension: tension,
 	}
-	cs.delta = 1 / float32(len(points))
+	cs.delta = 1 / float64(len(points)-1)
 	return cs
 }
 
 type CardinalSpline struct {
-	points  []*image.Point
-	tension float32
-	delta   float32
+	points  []*FPoint
+	tension float64
+	delta   float64
 }
 
-func (m *CardinalSpline) GetPoint(idx int) *image.Point {
+func (m *CardinalSpline) GetPoint(idx int) *FPoint {
 	if idx < 0 {
 		idx = 0
 	} else if idx > len(m.points)-1 {
@@ -31,7 +32,7 @@ func (m *CardinalSpline) GetPoint(idx int) *image.Point {
 	return m.points[idx]
 }
 
-func (m *CardinalSpline) GetCardinalSplineAt(idx1, idx2, idx3, idx4 int, t float32) *image.Point {
+func (m *CardinalSpline) GetCardinalSplineAt(idx1, idx2, idx3, idx4 int, t float64) *FPoint {
 	t2 := t * t
 	t3 := t2 * t
 	// log.D("GetCardinalSplineAt, s: %v, t: %v, t2: %v, t3: %v", s, t, t2, t3)
@@ -48,20 +49,20 @@ func (m *CardinalSpline) GetCardinalSplineAt(idx1, idx2, idx3, idx4 int, t float
 	b3 := m.tension*(t3-2*t2+t) + (-2*t3 + 3*t2)
 	b4 := m.tension * (t3 - t2)
 
-	x := float32(p1.X)*b1 + float32(p2.X)*b2 + float32(p3.X)*b3 + float32(p4.X)*b4
-	y := float32(p1.Y)*b1 + float32(p2.Y)*b2 + float32(p3.Y)*b3 + float32(p4.Y)*b4
+	x := p1.X*b1 + p2.X*b2 + p3.X*b3 + p4.X*b4
+	y := p1.Y*b1 + p2.Y*b2 + p3.Y*b3 + p4.Y*b4
 
 	// log.D("GetCardinalSplineAt, b1: %v, b2: %v, b3: %v, b4: %v", b1, b2, b3, b4)
 
-	return &image.Point{
-		X: int(x),
-		Y: int(y),
+	return &FPoint{
+		X: x,
+		Y: y,
 	}
 }
 
-func (m *CardinalSpline) GetNewPos(percent float32) *image.Point {
+func (m *CardinalSpline) GetNewPos(percent float64) *FPoint {
 	idx := 0
-	lt := float32(0)
+	lt := float64(0)
 	if percent < 0 {
 		percent = 0
 	}
@@ -71,7 +72,7 @@ func (m *CardinalSpline) GetNewPos(percent float32) *image.Point {
 	} else {
 		fIdx := percent / m.delta
 		idx = int(fIdx)
-		lt = fIdx - float32(idx)
+		lt = fIdx - float64(idx)
 	}
 	pos := m.GetCardinalSplineAt(idx-1, idx, idx+1, idx+2, lt)
 	// log.D("dt: %v, p: %v, lt: %v, x: %v, y: %v", percent, idx, lt, pos.X, pos.Y)
